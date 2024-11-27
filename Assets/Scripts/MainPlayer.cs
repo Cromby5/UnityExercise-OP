0fc916dbf364aec0bfbe9be1c5c15fe3fbf43810
Ben Reid <70808579+Cromby5@users.noreply.github.com>
2024-11-27 21:28:13 +0000

    Player Movement
    The player moves at a constant speed through the set points,

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MainPlayer : MonoBehaviour
 {
@@ -19,39 +20,69 @@ public class MainPlayer : MonoBehaviour
     [SerializeField] private AudioClip gameOverSound;
     [SerializeField] private ParticleSystem particle;
 
+    [SerializeField] private GameObject playerBody;
+
+    bool isGameOver = false;
+    public UnityEvent gameOverEvent; // may be a bit much for this example, but is useful for other objects / scripts to listen for.
+
     private void Awake()
     {
         audioSource = GetComponentInChildren<AudioSource>();
     }
 
-    void Start()
+    private void Start()
     {
-        
+        if (gameOverEvent == null)
+            gameOverEvent = new UnityEvent();
+
+        gameOverEvent.AddListener(GameOverHandle);
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (isGameOver)
+            return;
+        // I was planning this to be in fixedupdate originally / may change movement force to rigidbody due to the line "Note that the game uses the physics system provided by Unity"
+        // Keeping it simple for now for the first prototype.
+        if (currentPointIndex < pointsToMoveTo.Count)
+        {
+            // Move the object towards the current point at a constant speed (2)
+            transform.position = Vector3.MoveTowards(transform.position, pointsToMoveTo[currentPointIndex].position, speed * Time.deltaTime);
+            // If the object reaches the current point, move to the next point (3)
+            if (transform.position == pointsToMoveTo[currentPointIndex].position)
+            {
+                currentPointIndex++;
+                Debug.Log("Current Point Index: " + currentPointIndex);
+            }
+        }   
+        else 
+        {
+            gameOverEvent.Invoke();
+        }
     }
 
     private void FixedUpdate()
     {
-        // Move the object towards the current point at a constant speed (2)
         
-        // If the object reaches the current point, move to the next point (3)
-
     }
 
     private void GameOverHandle()
     {
-
+        Debug.Log("Game Over");
+        isGameOver = true;
+        audioSource.PlayOneShot(gameOverSound);
+        particle.Play();
+        Destroy(playerBody);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collision with: " + collision.gameObject.name);
         // If the object collides with an asteroid, destroy the object (4)
-
-        // If the object reaches the final point, destroy the object (4)
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            gameOverEvent.Invoke();
+        }
     }
 
     private void OnDestroy()
